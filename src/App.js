66d3e6c5df7c8ba5e1bf8fe348f8ef9b3578723c
benzwjian <zwjian@gmail.com>
2018-01-05 13:56:51 +0800

    add present and container element

--- src/App.js
+++ src/App.js
@@ -114,56 +114,51 @@ const initialState = {
 
 const store = createStore(reducer)
 
-class App extends Component {
+const App = () => ( //presentational stateless component
+  <div className='ui segment'>
+    <ThreadTabs />
+    <ThreadDisplay />
+  </div>
+)
+
+class ThreadTabs extends Component { //container component
   componentDidMount() {
     store.subscribe(() => this.forceUpdate())
   }
 
   render() {
     const state = store.getState()
-    const activeThreadId = state.activeThreadId
-    const threads = state.threads
-    const activeThread = threads.find((t) => t.id === activeThreadId)
-    const tabs = threads.map((t) => (
+    const tabs = state.threads.map(t => (
       {
         title: t.title,
-        active: t.id === activeThreadId,
+        active: t.id === state.activeThreadId,
         id: t.id
       }
     ))
-
     return (
-      <div className='ui segment'>
-        <ThreadTabs tabs={tabs} />
-        <Thread thread={activeThread} />
-      </div>
+      <Tabs tabs={tabs} onClick={(id) => (
+        store.dispatch({
+          type: 'OPEN_THREAD',
+          id: id
+        })
+      )} />
     )
   }
 }
 
-class ThreadTabs extends Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'OPEN_THREAD',
-      id: id
-    })
-  }
+const Tabs = (props) => ( //presentational stateless component
+  <div className='ui top attached tabular menu'>
+    {
+      props.tabs.map((tab, index) => (
+        <div key={index} className={tab.active ? 'active item' : 'item'} onClick={() => props.onClick(tab.id)}>
+          {tab.title}
+        </div>
+      ))
+    }
+  </div>
+)
 
-  render() {
-    const tabs = this.props.tabs.map((tab, index) => (
-      <div key={index} className={tab.active ? 'active item' : 'item'} onClick={() => this.handleClick(tab.id)}>
-        {tab.title}
-      </div>
-    ))
-    return (
-      <div className='ui top attached tabular menu'>
-        {tabs}
-      </div>
-    )
-  }
-}
-
-class MessageInput extends Component {
+class TextFieldSubmit extends Component { //presentational stateless component as well as controlled component
   state = {
     value: ''
   }
@@ -175,11 +170,7 @@ class MessageInput extends Component {
   }
 
   handleSubmit = () => {
-    store.dispatch({
-      type: 'ADD_MESSAGE',
-      text: this.state.value,
-      threadId: this.props.threadId
-    })
+    this.props.onSubmit(this.state.value)
     this.setState({
       value: ''
     })
@@ -197,30 +188,54 @@ class MessageInput extends Component {
   }
 }
 
-class Thread extends Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'DELETE_MESSAGE',
-      id: id
-    })
+const MessageList = (props) => ( //presentational stateless component
+  <div className='ui comments'>
+    {
+      props.messages.map((m, index) => (
+        <div className='comment' key={index} onClick={() => props.onClick(m.id)}>
+          <div className='text'>
+            {m.text}
+            <span className='metadata'>@{m.timestamp}</span>
+          </div>
+        </div>
+      ))
+    }
+  </div>
+)
+
+const Thread = (props) => ( //presentational stateless component
+  <div className='ui center aligned basic segment'>
+    <MessageList messages={props.thread.messages} onClick={props.onMessageClick} />
+    <TextFieldSubmit onSubmit={props.onMessageSubmit} />
+  </div>
+)
+
+class ThreadDisplay extends Component { //container component
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
   }
 
   render() {
-    const messages = this.props.thread.messages.map((message, index) => (
-      <div className='comment' key={index} onClick={() => this.handleClick(message.id)}>
-        <div className='text'>
-          {message.text}
-          <span className='metadata'>@{message.timestamp}</span>
-        </div>
-      </div>
-    ))
+    const state = store.getState()
+    const activeThreadId = state.activeThreadId
+    const activeThread = state.threads.find((t) => t.id === activeThreadId)
+
     return (
-      <div className='ui center aligned basic segment'>
-        <div className='ui comments'>
-          {messages}
-        </div>
-        <MessageInput threadId={this.props.thread.id} />
-      </div>
+      <Thread
+        thread={activeThread}
+        onMessageClick={(id) => (
+          store.dispatch({
+            type: 'DELETE_MESSAGE',
+            id: id
+          })
+        )}
+        onMessageSubmit={(text) => (
+          store.dispatch({
+            type: 'ADD_MESSAGE',
+            text: text,
+            threadId: activeThreadId
+          })
+        )} />
     )
   }
 }
